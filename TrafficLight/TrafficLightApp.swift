8b6f6ac56fc0d2880178cfedce2767be009b4864
Alexey Isaev <montilarard@gmail.com>
2023-03-13 20:52:17 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Алексей Исаев on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
